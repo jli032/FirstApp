4e2bbca819cd40f9bf7b9a12f823061fab78248c
Jeffrey Li <jli032@ucr.edu>
2018-02-11 10:20:32 -0800

    Fix result window bug that couldn't display result. The value it was getting was an integer instead of an expected string.
    Moved result window so the number pad window will not overlap the result window.

--- app/src/main/java/com/example/supremegoldieking/firstapp/MainActivity.java
+++ app/src/main/java/com/example/supremegoldieking/firstapp/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.supremegoldieking.firstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public static String emptyString = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addButton = findViewById(R.id.addButton);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+                    public void onClick(View view) {
+                        EditText inputFirstNum = findViewById(R.id.inputFirstNum);
+                        EditText inputSecondNum = findViewById(R.id.inputSecondNum);
+                        TextView resultTextView = findViewById(R.id.resultTextView);
+
+                        int num1 = Integer.parseInt(inputFirstNum.getText().toString());
+                        int num2 = Integer.parseInt(inputSecondNum.getText().toString());
+                        int result = num1 + num2;
+                        String resultString = result + emptyString;
+                        resultTextView.setText(resultString);
+            }
+
+        });
     }
 }
